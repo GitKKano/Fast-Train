ec70ef9d73cf0f46955f313947c82dc4b94a59c6
KKano <GitKKano@users.noreply.github.com>
2017-08-07 09:42:50 +0900

    Find a Good Train

---
+++ Fast Train/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Fast Train
+//
+//  Created by Kankuro Kano on 2017/08/04.
+//  Copyright © 2017 Kankuro Kano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var searchST: UISearchBar!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func Main() {
+        
+    }
+
+}
+
