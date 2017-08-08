1fffffb3b1e800d3e1f91e700d178517c2baf144
KKano <GitKKano@users.noreply.github.com>
2017-08-08 11:19:11 +0900

    Put API

--- Fast Train/AppDelegate.swift
+++ Fast Train/AppDelegate.swift
@@ -9,9 +9,11 @@
 import UIKit
 import GoogleMaps
 
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'Fast Train' do
+pod 'GoogleMaps'
+pod 'Google-Maps-iOS-Utils'
+end
+
